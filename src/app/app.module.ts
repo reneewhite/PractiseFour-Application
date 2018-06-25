554fd8ed42a1f0bb244d92c877a372070294f284
reneewhite <reneilwesefora@gmail.com>
2018-06-25 14:45:32 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { DetailPageComponent } from './detail-page/detail-page.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AboutComponent,
+    DetailPageComponent,
+    StatisticsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
